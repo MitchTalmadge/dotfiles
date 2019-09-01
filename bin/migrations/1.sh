9936f6883c742fd5b2c3991adfc1aded83ac3099
Mitch Talmadge <mitch@mitchtalmadge.com>
2019-09-01 01:19:29 -0600

    Add migration util function

--- bin/migrations/1.sh
+++ bin/migrations/1.sh
@@ -1,38 +1,29 @@
 #!/bin/sh
+. ~/.dotfiles/bin/migrations/.migrate-utils
 
 # Shells
 # ------------------------------------
 
 # csh
-cp ~/.login ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/csh/.login  ~/.login
-cp ~/.cshrc ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/csh/.cshrc ~/.cshrc
+backup_and_link ~/.dotfiles/shells/csh/.login ~/.login
+backup_and_link ~/.dotfiles/shells/csh/.cshrc ~/.cshrc
 
 # tcsh
-cp ~/.tcshrc ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/tcsh/.tcshrc ~/.tcshrc
+backup_and_link ~/.dotfiles/shells/tcsh/.tcshrc ~/.tcshrc
 
 # sh/bourne
-cp ~/.profile ~/.dotfiles/backups/ || true
-
-ln -sf ~/.dotfiles/shells/sh/.profile ~/.profile
+backup_and_link ~/.dotfiles/shells/sh/.profile ~/.profile
 
 # bash
-cp ~/.bash_profile ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/bash/.bash_profile ~/.bash_profile
-cp ~/.bashrc ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/bash/.bashrc ~/.bashrc
+backup_and_link ~/.dotfiles/shells/bash/.bash_profile ~/.bash_profile
+backup_and_link ~/.dotfiles/shells/bash/.bashrc ~/.bashrc
 
 # zsh
-cp ~/.zprofile ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/zsh/.zprofile ~/.zprofile
-cp ~/.zshrc ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/shells/zsh/.zshrc ~/.zshrc
+backup_and_link ~/.dotfiles/shells/zsh/.zprofile ~/.zprofile
+backup_and_link ~/.dotfiles/shells/zsh/.zshrc ~/.zshrc
 
 # Tools
 # ------------------------------------
 
 # tmux
-cp ~/.tmux.conf ~/.dotfiles/backups/ || true
-ln -sf ~/.dotfiles/tools/tmux/.tmux.conf ~/.tmux.conf
+backup_and_link ~/.dotfiles/tools/tmux/.tmux.conf ~/.tmux.conf
