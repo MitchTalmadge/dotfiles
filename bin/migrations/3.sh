df8838e73d389ab80c056f1363080cc1fe811593
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-06-11 07:24:17 +0000

    Add .vimrc and improve migration script.

---
+++ bin/migrations/3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+. ~/.dotfiles/bin/migrations/.migrate-utils
+
+# vim
+backup_and_link ~/.dotfiles/tools/vim/.vimrc ~/.vimrc
--- bin/migrations/migrate.sh
+++ bin/migrations/migrate.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+set +o xtrace
 
-CURRENT_VERSION=`cat ~/.dotfiles/bin/migrations/current_version`
+# Determine version from biggest migration name.
+VERSION=0
+for entry in `ls . | grep -E "^[0-9]+\.sh$"`
+do
+  ENTRY_VER=`echo $entry | grep -oE "[0-9]+"`
+  if [ "$ENTRY_VER" -gt "$VERSION" ]; then
+    VERSION=$ENTRY_VER
+  fi
+done
 
 if [ -z "$DOTFILES_VERSION" ]
 then
 	DOTFILES_VERSION=0
 fi
 
-while [ "$DOTFILES_VERSION" -lt "$CURRENT_VERSION" ]
+while [ "$DOTFILES_VERSION" -lt "$VERSION" ]
 do 
-	echo "Migrating from Version $DOTFILES_VERSION to Version $CURRENT_VERSION"
+	echo "Migrating from Version $DOTFILES_VERSION to Version $VERSION"
 	DOTFILES_VERSION=$((DOTFILES_VERSION + 1)) && \
 	sh ~/.dotfiles/bin/migrations/$DOTFILES_VERSION.sh && \
 	echo "Done!"
