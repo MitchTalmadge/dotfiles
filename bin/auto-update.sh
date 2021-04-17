e6aae8f2ce00821b62e5c9d961831ac1098c6e09
Mitch Talmadge <mitch@talmad.ge>
2021-04-17 13:10:34 -0600

    Update submodules on every update

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -2,5 +2,5 @@
 
 if [ -z "$DOTFILES_UPDATED" ]; then
 	echo "> Updating dotfiles..."
-	(cd ~/.dotfiles && git pull --rebase origin master && sh bin/migrations/migrate.sh)
+	(cd ~/.dotfiles && git pull --rebase origin master && git submodule update --init && sh bin/migrations/migrate.sh)
 fi
