73ec07874a7036a082a828d96118a9add52100fd
Mitch Talmadge <mitch@talmad.ge>
2021-11-11 06:46:01 +0000

    Fix silencing for auto-update when non-interactive

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 case "$DOTFILES_INTERACTIVE" in
-true) exec &>/dev/null ;; # Suppress all output for this script if not interactive
+"false") exec >/dev/null 2>/dev/null ;; # Suppress all output for this script if not interactive
 esac
 
 if [ -z "$DOTFILES_UPDATED" ]; then
 	echo "> Updating dotfiles..."
 	( 
-		
 		cd ~/.dotfiles 
 		git pull --rebase origin master \
 		&& git submodule update --init \
