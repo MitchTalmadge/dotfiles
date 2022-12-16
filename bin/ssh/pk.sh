d83d428af3d4f110fdf1ce0f777edc55feb3472e
Mitch Talmadge <8848080+MitchTalmadge@users.noreply.github.com>
2022-12-15 22:44:19 -0700

    Delete pk.sh

--- bin/ssh/pk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Yes I am aware that obfuscation is not security. It's to limit keyword searching on GitHub.
-FILE=$(echo "vq_efn" | tr '[n-za-m]' '[a-z]')
-mkdir -p ~/.ssh
-touch ~/.ssh/${FILE}
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/${FILE}
-cat ~/.dotfiles/bin/ssh/pk | tr '[N-ZA-M]' '[A-Z]' | base64 -d > ~/.ssh/${FILE}
