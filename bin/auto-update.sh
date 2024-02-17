10cbed273eb57eeb1a6d0d4ca23d5697fe24e2dc
Mitch Talmadge <mitch@talmad.ge>
2024-02-17 01:31:35 -0700

    Disable shell output

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+. ~/.dotfiles/bin/utils/echo
 
 case "$DOTFILES_INTERACTIVE" in
 "false") exec >/dev/null 2>/dev/null ;; # Suppress all output for this script if not interactive
 esac
 
 if [ -z "$DOTFILES_UPDATED" ]; then
-	echo "> Updating dotfiles..."
+	echoi "> Updating dotfiles..."
 	( 
 		cd ~/.dotfiles 
 		git pull --rebase origin master \
