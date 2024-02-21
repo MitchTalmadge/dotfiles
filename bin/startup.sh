bf1dd737dfd3ba30b6dafe35160307ce528738b7
Mitch Talmadge <mitch@talmad.ge>
2024-02-21 02:53:12 -0700

    Install rust compiler

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
+{
+  df_startup() {
+    echo "> Running Dotfiles Startup"
+    df_install_rust
+  }
 
-# Not used right now... runs on startup for all shells.
+  df_install_rust() {
+    if [ -f ~/.cargo/bin/rustc ]; then 
+      echo "Rust is installed."
+      return
+    fi
+
+    echo "Rust is not installed. Installing..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  }
+
+  df_startup
+} >> /tmp/df-log 2>&1
